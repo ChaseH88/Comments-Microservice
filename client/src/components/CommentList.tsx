04174f2b55274b17bc26b2bf2d97c78411aed457
ChaseH88 <ChaseH88@gmail.com>
2021-04-11 17:36:33 -0500

    updated client app with new comment status param

--- client/src/components/CommentList.tsx
+++ client/src/components/CommentList.tsx
@@ -1,20 +1,45 @@
 import React, { FC } from 'react';
+import config from '../../public/config.json';
 
 interface CommentListInterface {
-  comments: any
+  comments: Comment[]
+}
+
+interface Comment {
+  id: string,
+  content: string,
+  status: string
 }
 
 const CommentList: FC<CommentListInterface> = ({ comments }): JSX.Element => {
 
+  const { commentStatus: { approved, rejected, pending } } = config;
+
+  /**
+   * Handle the comment's status
+   */
+  const handleStatus = ({ content, status }: Comment) => {
+    return (() => {
+      switch (status) {
+        case approved: return content;
+        case pending: return 'Comment is currently under review.';
+        case rejected: return 'This comment has been blocked.';
+        default: return '';
+      }
+    })();
+  }
+
   const renderComments = (): JSX.Element => (
     <ul>
-      {comments?.map(({ id, content }: { id: string, content: string }) => (
-        <li key={id}>{content}</li>
+      {comments?.map((comment: Comment) => (
+        <li key={comment.id} className={comment.status}>
+          {handleStatus(comment)}
+        </li>
       ))}
     </ul>
   );
 
-  return(
+  return (
     <>
       <h2>Post</h2>
       {!!comments?.length && renderComments()}
--- client/src/hooks/useAxios.ts
+++ client/src/hooks/useAxios.ts
@@ -5,6 +5,7 @@ const useAxios = async (
   method: 'get' | 'post' = 'get',
   data: { [key: string]: any } = {}
 ) => (
+  //@ts-ignore <-- no error here just weird ts info in terminal
   await axios[method](path, data)
 );
 
